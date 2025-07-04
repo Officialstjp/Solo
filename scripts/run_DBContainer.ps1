08fa6d7cc87e21168f679b40217537bfb306eb3d
OfficialStjp <stefanploch00@gmail.com>
2025-07-04 23:15:58 +0200

    restructure

--- scripts/run_DBContainer.ps1
+++
@@ -1,8 +0,0 @@
-try {
-    Write-Host "Starting PostgreSQL container..."
-    cmd.exe /c  "docker run --name SoloPostGres -e POSTGRES_PASSWORD=J*bApp7ic4tion -p 5431:5432 -d postgres"
-    Write-Host "Container started successfully." -ForegroundColor Green
-}
-catch {
-    Write-Host "Failed to start container: `n$_"
-}
