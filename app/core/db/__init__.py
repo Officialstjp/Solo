ab1f6d45a21da56bf30de4b3b351fe91aea695e5
OfficialStjp <stefanploch00@gmail.com>
2025-07-06 15:10:36 +0200

    prepare next implementatons

---
+++ app/core/db/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module: app/core/db/__init__.py
+Purpose: Database package initialization
+"""
+
+# Import main classes for easier access
+from app.core.db.connection import get_connection_pool, get_sync_connection, close_pool
